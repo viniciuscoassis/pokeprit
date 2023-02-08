74de15baae0709bd8e5900bbf9d632653bb844ed
vinicius <vinicius.coassis@gmail.com>
2023-02-08 17:33:17 -0300

    feat: add all routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { ToastContainer } from "react-toastify";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashBoard from "./pages/DashBoard/Dashboard";
 import HomePage from "./pages/DashBoard/Home/HomePage";
+import PokemonSelectionPage from "./pages/DashBoard/PokemonSelection/PokemonSelection";
+import DateSelectionPage from "./pages/DashBoard/DateSelection/DateSelection";
+import TimeSelectionPage from "./pages/DashBoard/TimeSelection/TimeSelection";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Routes>
           <Route path="/" element={<DashBoard />}>
             <Route path="home" element={<HomePage />} />
+            <Route path="selection" element={<PokemonSelectionPage />} />
+            <Route path="day" element={<DateSelectionPage />} />
+            <Route path="hour" element={<TimeSelectionPage />} />
             <Route index path="*" element={<HomePage />} />
           </Route>
         </Routes>
